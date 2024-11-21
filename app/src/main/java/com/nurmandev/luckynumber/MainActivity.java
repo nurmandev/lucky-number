1bd484383aec2ed50e29031578ba5b78c4cb4fb0
nurmandev <adebayour66265@gmail.com>
2024-11-21 23:08:46 +0100

    update

--- app/src/main/java/com/nurmandev/luckynumber/MainActivity.java
+++ app/src/main/java/com/nurmandev/luckynumber/MainActivity.java
@@ -1,25 +1,39 @@
 package com.nurmandev.luckynumber;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+    editText = findViewById(R.id.user_name);
+    textView = findViewById(R.id.lucky);
+    button = findViewById(R.id.openBrowser);
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
+
     }
 
 }

67b9925fa78395cd88e7e41afcebbcc9b29590a0
nurmandev <adebayour66265@gmail.com>
2024-11-22 14:57:16 +0100

    update

--- app/src/main/java/com/nurmandev/luckynumber/MainActivity.java
+++ app/src/main/java/com/nurmandev/luckynumber/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nurmandev.luckynumber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
     button.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            String userName = editText.getText().toString();
 
+            Intent intent = new Intent(
+                    getApplicationContext(), SecondActivity.class
+            );
+
+            intent.putExtra("name", userName);
+
+            startActivity(intent);
         }
     });
 
